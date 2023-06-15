bd323500eb18c2bfb266b8baa9f0582cb21b0952
Sangeet Sharma <sangeetsharmaofficial@gmail.com>
2023-06-16 02:31:50 +0530

    learnt basics of node.js

---
+++ _01_basics/_01_nodejs.js
@@ -0,0 +1,26 @@
+// node js ------------
+
+/*
+Node.js is  a javascript runtime built on chrome,s  V8 engine.
+Node.js is a javascript running on the server.
+
+Every browser contains an engine which converts javasccript to machine understanding language.
+
+V8 is the javascript engine used by google chrome.
+
+in 2009, Ryan Dahl took V8 engine and run a C++ file and named it node.exe   which was than called nodejs.
+
+
+When to use node js ?
+
+I/O bound -> When you have to work with multiple requests at same time.Node js is non blocking even though it is single threaded
+
+Data Streaming Applications -> When you have a lot of never ending data
+
+Real time chat applications
+
+
+When not to use node.js ?
+
+We should avoid using node js while making CPU intensive applications.
+*/
