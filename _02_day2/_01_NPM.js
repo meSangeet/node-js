4ebbcbcb2d4bfcff395ca6da646aae0ab80c8ada
Sangeet Sharma <sangeetsharmaofficial@gmail.com>
2023-06-17 10:07:45 +0530

    learnt about npm

---
+++ _02_day2/_01_NPM.js
@@ -0,0 +1,27 @@
+// npm ----------node package manager----
+
+/*
+
+node package manager is the default package manager for the javascript runtime environment node.js.
+
+What is package.json  ?
+
+whatever packages you have installed from npm all the details are stored in package.json in the form of dependencies.
+
+all the extra information about the packages are stored inside package-lock.json
+
+then you also need to require the module
+
+eg in command
+
+npm init chalk
+
+then 
+
+const chalk = require('chalk');
+
+
+note -> validator is a npde package which can be used for form validation.
+note -> in the JSON file both the property and its values are written inside the double quotes.
+
+*/
