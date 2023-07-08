f0f5aa528151b366248f0b2d45d0e2a36b27a07a
Sangeet Sharma <sangeetsharmaofficial@gmail.com>
2023-07-09 03:57:02 +0530

    how to change the name of views directory which is mandatory to use template engines

---
+++ _07_day7/_02_customizingViewsDirectory.js
@@ -0,0 +1,5 @@
+//to use template engines we need to have a directory with the name views. So let's say we need to change the file name views to something other.
+
+//syntax
+
+app.set('views', '<path of the directory>');
